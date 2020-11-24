0ae54c038c25d54b168779fdc2cf236b644de2c3
alex Rodríguez <alexrodriguezlop@correo.ugr.es>
2020-11-24 09:50:24 +0100

    Probando otra forma de obtener el parámetro HU4 #4

--- api/app.js
+++ api/app.js
@@ -26,11 +26,12 @@ module.exports = (req, res) => {
 
   if(parametro != null){
     result = obtener(md5(parametro));
-    res.status(200).send(result);
+    
 
     if(result === null){
       res.status(404).send("No se encontró."); 
     }
+    res.status(200).send(result);
   }
   else {
     res.status(400).send('Formato incorrecto, PRUEBE:?msg="test".');
