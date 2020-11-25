0a766837bc745c8b67216a732389a39865d6c736
alex Rodríguez <alexrodriguezlop@correo.ugr.es>
2020-11-25 11:18:20 +0100

    Creado Bot de Telegram #5

---
+++ api/appBot.js
@@ -0,0 +1,40 @@
+const data = require("../data/data.json" )
+const  md5 = require('md5');
+
+
+// Extrae una tupla del fichero JSON a partir de una clave hash 
+function obtener(valor){
+  var result = null;
+
+  for(var i = 0; i < data.lista.length; i++){
+    if(data.lista[i].hash === valor)
+     result = data.lista[i];
+  }
+  return result;
+}
+
+
+// Función principal manejadora de petición
+module.exports = (req, res) => {
+
+  if (typeof(req.body) !== 'undefined'){
+
+    const chatID = req.body.message.chat.id;
+    const sentMessage = req.body.message.text;
+    var parametro = null;
+
+    if(parametro != null){
+      result = obtener(md5(parametro));
+
+      if(result === null){
+        res.status(404).send("No se encontró."); 
+      }
+      else{
+        res.status(200).send(result);
+      }
+      
+    }
+
+  }
+}
+
