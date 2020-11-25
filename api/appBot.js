8d6bb393c4498046fc94d1b41788c019380359dd
alex Rodríguez <alexrodriguezlop@correo.ugr.es>
2020-11-25 20:55:16 +0100

    Rehaciendo con telegraf Bot de Telegram #5

--- api/appBot.js
+++ api/appBot.js
@@ -15,11 +15,14 @@ function obtener(valor){
 
 
 const token = process.env.BOT_TOKEN;
-const { Telegraf } = require('telegraf')
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => {
+  ctx.reply('Welcome')
+});
+
+module.exports = async (req, res) => {
+  
+}
