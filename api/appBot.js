5f13ebf30413ba7fccbba8a5305aae7e22bbfee2
alex Rodríguez <alexrodriguezlop@correo.ugr.es>
2020-11-27 13:41:07 +0100

    Corrigiendo Bot de Telegram #5

--- api/appBot.js
+++ api/appBot.js
@@ -1,16 +1,65 @@
+const data = require("../data/data.json" )
+const  md5 = require('md5');
+
+
+// Extrae una tupla del fichero JSON a partir de una clave hash 
+function obtener(valor){
+  var result = null;
+
+  for(var i = 0; i < data.lista.length; i++){
+    if(data.lista[i].hash === valor)
+     result = data.lista[i];
+  }
+  return result;
+}
 
 // Función principal manejadora de petición
 module.exports = async (req, res) => {
-  /*
+  if(req.body.message != undefined){
+    console.log('A' + req.body.message);
     const chatID = req.body.message.chat.id;
     const msgID = req.body.message.message_id;
-*/
-    console.log(req.body.message);
-    const chatID = req.body.message.chat.id;
-    const msgID = req.body.message.message_id;
+    const cadena = req.body.message.text;
+  }
+  else{
+    console.log('B' + req.body.edited_message);
+    const chatID = req.body.edited_message.chat.id;
+    console.log(chatID);
+    const msgID = req.body.edited_message.message_id;
+    console.log(msgID);
+    const cadena = req.body.edited_message.text;
+    console.log(cadena);
+  }
+    const arg = cadena.slice(0, 7).toLowerCase();
+    const clave = cadena.slice(7, cadena.length); 
 
     var mensaje = '';
 
+    switch(arg) {
+      // /BUSCAR
+      case '/buscar':
+        if(clave != ''){
+          var result = obtener(md5(clave));
+    
+          if(result === null){
+            mensaje = '*Su mensaje no ha sido cifrado y por tanto no hay registros*';
+          }
+          else{
+            mensaje = '*Fecha:* ' + result.fecha + ' *Hora:* ' + result.hora;
+          }
+        }
+        else{
+          mensaje = 'No ha introducido el texto a buscar';
+        }
+        break;
+      // /HELP
+      case '/help':
+        mensaje = 'Para buscar usa la orden * /buscar* seguida del texto';
+        break;
+      // Otro caso    
+      default:
+        mensaje = 'Comando desconocido, use * /help*';
+    }
 
     const telegramRes = {
       text:mensaje, 
