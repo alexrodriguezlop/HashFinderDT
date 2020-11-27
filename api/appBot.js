6d19983a11fc4551d3647181cbf0b9e86f584051
alex Rodríguez <alexrodriguezlop@correo.ugr.es>
2020-11-27 09:38:18 +0100

    Corrigiendo Bot de Telegram #5

--- api/appBot.js
+++ api/appBot.js
@@ -16,37 +16,6 @@ function obtener(valor){
 
 // Función principal manejadora de petición
 module.exports = async (req, res) => {
-  console.log('Versión');
-  console.log(req.body);
 
-  if(req.body.message != undefined){
-    const chatID = req.body.message.chat.id;
-    const msgID = req.body.message.message_id;
-    const cadena = req.body.message.text;
-    const arg = cadena.slice(0, 7).toLowerCase();
-    const clave = cadena.slice(7, cadena.length); 
-    var mensaje = '';
-
-    console.log(req.body.message);
-    console.log(arg);
-    console.log(clave);
-
-    console.log(req.body.message);
-
-
-
-
-
-    const telegramRes = {
-      text:mensaje, 
-      method:"sendMessage", 
-      chat_id:chatID, 
-      reply_to_message_id: msgID, 
-      parse_mode: 'Markdown'
-    };
-
-    res.setHeader("Content-Type","application/json");
-    res.status(200).json(telegramRes);
-  }
   res.status(200);
 }
