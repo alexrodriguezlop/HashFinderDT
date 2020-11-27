73b3ed6ae75dbc003d0e8c6a057bb5a811f3fb59
alex Rodríguez <alexrodriguezlop@correo.ugr.es>
2020-11-27 10:15:25 +0100

    Corrigiendo Bot de Telegram #5

--- api/appBot.js
+++ api/appBot.js
@@ -1,27 +1,15 @@
-const data = require("../data/data.json" )
-const  md5 = require('md5');
-
-
-// Extrae una tupla del fichero JSON a partir de una clave hash 
-function obtener(valor){
-  var result = null;
-
-  for(var i = 0; i < data.lista.length; i++){
-    if(data.lista[i].hash === valor)
-     result = data.lista[i];
-  }
-  return result;
-}
-
 
 // Función principal manejadora de petición
 module.exports = async (req, res) => {
-
-    console.log('B' + req.body.edited_message);
+  /*
+    const chatID = req.body.message.chat.id;
+    const msgID = req.body.message.message_id;
+*/
     const chatID = req.body.edited_message.chat.id;
     const msgID = req.body.edited_message.message_id;
 
     var mensaje = '';
+
 
     const telegramRes = {
       text:mensaje, 
