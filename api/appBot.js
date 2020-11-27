b0165731a21f5bbc3084c5c467d0e5f38708af6f
alex Rodríguez <alexrodriguezlop@correo.ugr.es>
2020-11-27 09:42:12 +0100

    Corrigiendo Bot de Telegram #5

--- api/appBot.js
+++ api/appBot.js
@@ -16,6 +16,58 @@ function obtener(valor){
 
 // Función principal manejadora de petición
 module.exports = async (req, res) => {
+  if(req.body.message != undefined){
+    const chatID = req.body.edited_message.chat.id;
+    const msgID = req.body.edited_message.message_id;
+    const cadena = req.body.edited_message.text;
+    const arg = cadena.slice(0, 7).toLowerCase();
+    const clave = cadena.slice(7, cadena.length); 
+    var mensaje = '';
 
+    console.log(req.body.message);
+    console.log(arg);
+    console.log(clave);
+
+    console.log(req.body.message);
+
+    switch(arg) {
+      // /BUSCAR
+      case '/buscar':
+        if(clave != ''){
+          var result = obtener(md5('test'));
+          console.log(result);
+
+          if(result === null){
+            mensaje = '*Su mensaje no ha sido cifrado y por tanto no hay registros*';
+          }
+          else{
+            mensaje = '*Fecha:* ' + result.fecha + ' *Hora:* ' + result.hora;
+          }
+        }
+        else{
+          mensaje = 'No ha introducido el texto a buscar';
+        }
+      break;
+      // /HELP
+      case '/help':
+        mensaje = 'Para buscar usa la orden */buscar* seguida del texto';
+      break;
+      // Otro caso    
+      default:
+        mensaje = 'Comando desconocido, use */help*';
+    }
+
+
+    const telegramRes = {
+      text:mensaje, 
+      method:"sendMessage", 
+      chat_id:chatID, 
+      reply_to_message_id: msgID, 
+      parse_mode: 'Markdown'
+    };
+
+    res.setHeader("Content-Type","application/json");
+    res.status(200).json(telegramRes);
+  }
   res.status(200);
 }
